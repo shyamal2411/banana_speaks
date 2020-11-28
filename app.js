b2eff367b1f41ee0fce6253967da7bc9d6ebd967
shyamal2411 <sgprajapati24@gmail.com>
2020-11-28 09:40:40 +0530

    added js file

---
+++ app.js
@@ -0,0 +1,14 @@
+/*console.log("Script is working from a different file");
+var username = prompt("Give me your username")
+alert("This script works!" + username)
+var welcomemessage ="This is working!" +username;
+
+
+//output
+alert(welcomemessage);*/
+var username = prompt("Please input username")
+
+var welcomemessage = "This is working!" + username;
+
+//output
+alert(welcomemessage);
